f7d1cfa68e050521dccec9c495f9fc135a7dd412
MarcioAlex-x <marcioalex586@gmail.com>
2025-08-05 23:51:56 -0300

    chore: initial project setup

---
+++ src/index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express')
+const conn = require('./database/index')
+const app = express()
+
+PORT = process.env.PORT
+DB_HOST = process.env.DB_HOST
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+app.get('/api',(req,res)=>{
+    res.json({message:'This app is ON.'})
+})
+
+conn.sync()
+    .then(()=>{
+        app.listen(PORT,()=>{
+            console.log('http://localhost:3000')
+        })
+    })
+    .catch(err=>{
+        console.log(err)
+    })
