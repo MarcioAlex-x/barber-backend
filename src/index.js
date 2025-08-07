a70cca7b2f88fe76f0bddef62f858c848c388c7a
MarcioAlex-x <marcioalex586@gmail.com>
2025-08-07 11:26:34 -0300

    Erro corrigido

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const conn = require('./database/index')
 const app = express()
 
+const userRoute = require('./routes/user.route')
+
 PORT = process.env.PORT
 DB_HOST = process.env.DB_HOST
 
@@ -12,6 +14,7 @@ app.use(express.json())
 app.get('/api',(req,res)=>{
     res.json({message:'This app is ON.'})
 })
+app.use(userRoute)
 
 conn.sync()
     .then(()=>{
