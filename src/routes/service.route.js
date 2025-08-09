3157b82a0086b0db2ccdc94380f06229c938e6d3
MarcioAlex-x <marcioalex586@gmail.com>
2025-08-09 13:56:04 -0300

    Camada JWT adicionada

--- src/routes/service.route.js
+++ src/routes/service.route.js
@@ -2,11 +2,12 @@ const express = require('express')
 const route = express.Router()
 
 const serviceController = require('../controllers/service.controller')
+const jwtPermitions = require('../services/security/services.security.auth-permissions')
 
 route.get('/services',serviceController.list)
 route.get('/services/:id',serviceController.show)
-route.post('/services',serviceController.create)
-route.put('/services/:id',serviceController.update)
-route.delete('/services/:id',serviceController.delete)
+route.post('/services', jwtPermitions.checkIsAdmin, serviceController.create)
+route.put('/services/:id', jwtPermitions.checkIsAdmin, serviceController.update)
+route.delete('/services/:id', jwtPermitions.checkIsAdmin, serviceController.delete)
 
 module.exports = route
